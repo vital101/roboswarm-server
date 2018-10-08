37eebddfaf5ba38918a61f71b8f8f68b7a228f73
Jack Slingerland <jack.slingerland@gmail.com>
2018-10-08 08:52:11 -0400

    Updating db for deb to be more flexible

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,17 @@
-// Update with your config settings.
+// Environment variables
+require("dotenv").config();
+
+const devDbConnection = {
+  host: process.env.DB_HOST || '10.0.2.2',
+  user: process.env.DB_USER || 'postgres',
+  database: 'roboswarm'
+};
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      database: 'roboswarm',
-    },
+    connection: devDbConnection,
     pool: {
       min: 2,
       max: 10
