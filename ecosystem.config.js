1f06c60935991ee91d719c1854ff58600106e6ac
Jack Slingerland <jack.slingerland@gmail.com>
2018-11-02 11:51:26 -0400

    WIP

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps : [{
+    name: 'RoboSwarm API',
+    script: 'dist/server.js',
+    instances: 2,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '256MB',
+    env: {
+      NODE_ENV: 'development',
+      TZ: 'utc'
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      TZ: 'utc'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '159.89.184.163',
+      ref  : 'origin/master',
+      repo : 'git@bitbucket.org:roboswarm/roboswarm-server.git',
+      path : '/var/www/roboswarm',
+      'post-deploy' : 'npm install && npm run migrate && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
