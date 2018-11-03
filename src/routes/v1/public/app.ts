a16e8533e4d39ce10bef930c191213aab1c747a5
Jack Slingerland <jack.slingerland@gmail.com>
2018-11-03 09:53:29 -0400

    Deploy

--- src/routes/v1/public/app.ts
+++ src/routes/v1/public/app.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 router.route("/")
     .get(async (req: express.Request, res: express.Response) => {
         res.status(200);
-        res.sendFile("/var/www/roboswarm-static/dist/index.html");
+        res.sendFile("/var/www/roboswarm-static/static/dist/index.html");
     });
 
 export default router;
