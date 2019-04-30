75e0e8a2537f900aa2eb7a0291cabe89778b5a4b
Jack Slingerland <jack.slingerland@gmail.com>
2019-04-30 19:35:08 -0400

    Swarm cleanup changes

--- src/cron/swarm_cleanup.ts
+++ src/cron/swarm_cleanup.ts
@@ -13,8 +13,18 @@ import { getPlan, Plan } from "../lib/config";
     for (const swarm of swarms) {
         const swarmUser: User.User = await User.getById(swarm.user_id);
         const plan: Plan = getPlan(swarmUser);
-        const now = moment();
-        const maxSwarmEnd = moment(swarm.created_at).add(plan.maxLoadTestDurationMinutes, "minutes");
+        const now: moment.Moment = moment();
+        let maxSwarmEnd: moment.Moment;
+
+        // If the number of users is under 25 odds are that this is a
+        // reliability swarm so we need to check for that instead of
+        // normal max load test duration.
+        if (swarm.simulated_users <= 25) {
+            maxSwarmEnd = moment(swarm.created_at).add(plan.maxReliabilityTestMinutes, "minutes");
+        } else {
+            maxSwarmEnd = moment(swarm.created_at).add(plan.maxLoadTestDurationMinutes, "minutes");
+        }
+
         if (now.isAfter(maxSwarmEnd)) {
             console.log("Destroying swarm ", swarm.name);
             await Swarm.destroyById(swarm.id, swarm.group_id);
