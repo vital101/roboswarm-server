d8291903077c8893cd1b406b5f745c584248e89d
Jack Slingerland <jack.slingerland@gmail.com>
2019-12-07 07:03:12 -0500

    Authorization test

--- test/lib/authorization.test.ts
+++ test/lib/authorization.test.ts
@@ -1,5 +1,8 @@
 import * as sinon from "sinon";
 import * as stripeHelpers from "../../src/lib/stripe";
+import * as authorization from "../../src/lib/authorization";
+import * as moment from "moment";
+import { User } from "../../src/models/User";
 
 describe("lib/authorization", () => {
     let sandbox: sinon.SinonSandbox;
@@ -13,37 +16,25 @@ describe("lib/authorization", () => {
 
     describe("getAuthorizationDateRange", () => {
         test("returns an authorization date range", async () => {
+            const testUser: User = {
+                email: "test@example.com",
+                first_name: "test",
+                last_name: "user",
+                is_delinquent: false
+            };
+            const end: number = 1575919680;
+            const start: number = 1575719680;
             const getUserSubscriptionStub = sandbox.stub(stripeHelpers, "getUserSubscription").resolves({
-                current_period_end: 123123,
-                current_period_start: 123123,
-                object: "subscription",
-                application_fee_percent: undefined,
-                billing: "charge_automatically",
-                collection_method: "charge_automatically",
-                billing_cycle_anchor: 0,
-                billing_thresholds: undefined,
-                cancel_at: undefined,
-                cancel_at_period_end: true,
-                canceled_at: undefined,
-                created: 0,
-                customer: "test",
-                days_until_due: undefined,
-                default_payment_method: "test",
-                default_source: "test",
-                default_tax_rates: [],
-                discount: undefined,
-                ended_at: undefined,
-                items,
-                latest_invoice: "test",
-                livemode: true,
-                metadata: {},
-                start: 0,
-                start_date: 0,
-                status: "active",
-                tax_percent: undefined,
-                trial_end: undefined,
-                trial_start: undefined,
-            });
+                current_period_end: end,
+                current_period_start: start
+            } as any);
+            const dateRange: authorization.DateRange = await authorization.getAuthorizationDateRange(testUser);
+            expect(getUserSubscriptionStub.callCount).toBe(1);
+            const startDate: Date = moment.unix(start).toDate();
+            const endDate: Date = moment.unix(end).toDate();
+            expect(startDate.getSeconds()).toBe(dateRange.start.getSeconds());
+            expect(endDate.getSeconds()).toBe(dateRange.end.getSeconds());
+
         });
     });
 });
