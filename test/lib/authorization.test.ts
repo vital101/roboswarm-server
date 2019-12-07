2d592158d84a839dc15cf61ff8b54cae314ac7b0
Jack Slingerland <jack.slingerland@gmail.com>
2019-12-07 07:25:48 -0500

    Exceed max machine minutes test

--- test/lib/authorization.test.ts
+++ test/lib/authorization.test.ts
@@ -2,6 +2,7 @@ import * as sinon from "sinon";
 import * as stripeHelpers from "../../src/lib/stripe";
 import * as authorization from "../../src/lib/authorization";
 import * as moment from "moment";
+import * as Swarm from "../../src/models/Swarm";
 import { User } from "../../src/models/User";
 
 describe("lib/authorization", () => {
@@ -35,6 +36,44 @@ describe("lib/authorization", () => {
             expect(startDate.getSeconds()).toBe(dateRange.start.getSeconds());
             expect(endDate.getSeconds()).toBe(dateRange.end.getSeconds());
 
+        });
+    });
+
+    describe("willExceedMaxMachineHours", () => {
+        let user: User;
+        let getUserSubscriptionStub: sinon.SinonStub;
+        let machinesInPeriodStub: sinon.SinonStub;
+
+        beforeEach(() => {
+            user = {
+                email: "test@example.com",
+                first_name: "test",
+                last_name: "user",
+                is_delinquent: false,
+                stripe_plan_description: "free",
+                group: { id: 1, name: "Test Group" }
+            };
+            const end: number = 1575919680;
+            const start: number = 1575719680;
+            getUserSubscriptionStub = sandbox.stub(stripeHelpers, "getUserSubscription").resolves({
+                current_period_end: end,
+                current_period_start: start
+            } as any);
+            machinesInPeriodStub = sandbox.stub(Swarm, "totalMachineSecondsInPeriod").resolves(10);
+        });
+
+        test("returns true if the test duration exceeds the max available machine hours", async () => {
+            const willExceedMaxMachineHours: boolean = await authorization.willExceedMaxMachineHours(user, 500);
+            expect(willExceedMaxMachineHours).toBe(true);
+            expect(getUserSubscriptionStub.callCount).toBe(1);
+            expect(machinesInPeriodStub.callCount).toBe(1);
+        });
+
+        test("returns false if the test duration does not exceed the max available machine hours", async () => {
+            const willExceedMaxMachineHours: boolean = await authorization.willExceedMaxMachineHours(user, 5);
+            expect(willExceedMaxMachineHours).toBe(false);
+            expect(getUserSubscriptionStub.callCount).toBe(1);
+            expect(machinesInPeriodStub.callCount).toBe(1);
         });
     });
 });
