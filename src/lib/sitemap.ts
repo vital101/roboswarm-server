2295adfc239ed08971a495753234ed52dc407a5e
Jack Slingerland <jack.slingerland@gmail.com>
2021-12-29 13:51:57 -0500

    sitemap-index.xml

--- src/lib/sitemap.ts
+++ src/lib/sitemap.ts
@@ -12,12 +12,35 @@ export async function getSitesFromSitemap(url: string): Promise<SitemapRoute[]> 
     } else {
         sitemapUrl = `${url}/sitemap.xml`;
     }
+
+    // Try regular sitemaps
     const sm = new SiteMapper({ url: sitemapUrl });
     const sites: SitemapperResponse = await sm.fetch();
-    return sites.sites.map(site => {
-        return {
-            path: site.replace(url, ""),
-            method: "GET"
-        };
-    });
+    if (sites.sites.length !== 0) {
+        return sites.sites.map(site => {
+            return {
+                path: site.replace(url, ""),
+                method: "GET"
+            };
+        });
+    } else {
+        const yoastUrl = `${url}/sitemap_index.xml`;
+        const smYoast = new SiteMapper({
+            url: yoastUrl,
+            concurrency: 1,
+            debug: true,
+            rejectUnauthorized: false,
+            timeout: 60000, // 1 minute
+            requestHeaders: {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0"
+            },
+        });
+        const yoastSites: SitemapperResponse = await smYoast.fetch();
+        return yoastSites.sites.map(site => {
+            return {
+                path: site.replace(url, ""),
+                method: "GET"
+            };
+        });
+    }
 }
