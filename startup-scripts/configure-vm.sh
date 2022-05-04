89f38973e594eed4888bbae921f7c071ee986090
Jack Slingerland <jack.slingerland@gmail.com>
2022-05-04 07:07:03 -0400

    WIP - Startup script

---
+++ startup-scripts/configure-vm.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Disable ipv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+# WIP -> Report back
+
+# Open the firewall for Locust
+ufw allow 8000:65535/tcp
+# WIP -> Report back
+
+# Wait for apt to become available.
+while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+   sleep 1
+done
+
+# Install necessary packages
+export DEBIAN_FRONTEND=noninteractive && apt update
+export DEBIAN_FRONTEND=noninteractive && apt upgrade -y
+export DEBIAN_FRONTEND=noninteractive && apt-get install -y python3-pip unzip traceroute emacs
+# WIP -> Report back
