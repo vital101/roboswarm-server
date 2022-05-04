149eed2153d4a1aaf5f5648218332dcb57668870
Jack Slingerland <jack.slingerland@gmail.com>
2022-05-04 07:07:03 -0400

    Removing NodeSSH dependency.

--- src/lib/setupHelpers.ts
+++ src/lib/setupHelpers.ts
@@ -1,7 +1,5 @@
 import { NodeSSH } from "node-ssh";
-const sftp_client = require("ssh2-sftp-client");
 import { execSync } from "child_process";
-import { writeFileSync } from "fs";
 import { asyncSleep } from "../lib/lib";
 import * as Machine from "../models/Machine";
 import * as SSHKey from "../models/SSHKey";
@@ -280,22 +278,12 @@ export async function processMachineProvisionEvent(event: MachineProvisionEvent)
                     await traceRoute(event.machine.id, event.machine.ip_address, event.swarm.host_url, event.sshKey.private);
                     break;
                 }
-                case MachineSetupStep.TRANSFER_FILE: {
-                    const filePath = await LoadTestFile.getLocalFilePathBySwarmId(event.swarm.id);
-                    await transferFileToMachine(event.machine.ip_address, filePath, event.sshKey.private);
-                    execSync(`rm ${filePath}`, { cwd: "/tmp" });
-                    break;
-                }
                 case MachineSetupStep.UNZIP_AND_PIP_INSTALL: {
                     await unzipPackageAndPipInstall(event.machine.id, event.machine.ip_address, event.sshKey.private);
                     break;
                 }
                 case MachineSetupStep.START_MASTER: {
-                    await startMaster(event.swarm, event.machine, event.slaveCount, event.slaveIds, event.sshKey.private);
-                    break;
-                }
-                case MachineSetupStep.START_SLAVE: {
-                    await startSlave(event.swarm, event.master, event.machine, event.sshKey.private);
+                    await startDataCollection(event.swarm, event.machine, event.slaveCount, event.slaveIds, event.sshKey.private);
                     break;
                 }
             }
@@ -326,19 +314,6 @@ export async function traceRoute(machineId: number, machineIp: string, hostUrl: 
     console.log(`Finished traceroute on ${machineIp} to ${domain.host}`);
 }
 
-export async function transferFileToMachine(machineIp: string, filePath: string, privateKey: string): Promise<void> {
-    console.log(`Starting: Transfer ${filePath} to ${machineIp} @ path /root/load_test_data.zip`);
-    const sftp = new sftp_client();
-    await sftp.connect({
-        host: machineIp,
-        port: 22,
-        username: "root",
-        privateKey
-    });
-    await sftp.put(filePath, "/root/load_test_data.zip");
-    console.log("Success transferring file " + filePath);
-}
-
 export async function unzipPackageAndPipInstall(machineId: number, machineIp: string, privateKey: string): Promise<void> {
     const ssh = new NodeSSH();
     console.log(`Starting pip install on ${machineIp}`);
@@ -361,72 +336,7 @@ export async function unzipPackageAndPipInstall(machineId: number, machineIp: st
     console.log(`Machine dependency_install_complete flag set ${machineIp}`);
 }
 
-export async function startMaster(swarm: Swarm.Swarm, machine: Machine.Machine, slaveCount: number, slaveIds: number[], privateKey: string): Promise<void> {
-    console.log(`Starting master at ${machine.ip_address}`);
-    const ssh = new NodeSSH();
-    await ssh.connect({
-        host: machine.ip_address,
-        username: "root",
-        privateKey,
-    });
-    const users = swarm.simulated_users;
-    const rate = swarm.spawn_rate;
-    const runTime = `${swarm.duration}m`;
-    const hostUrl: string = swarm.host_url[swarm.host_url.length - 1] === "/" ?
-        swarm.host_url.slice(0, -1) :
-        swarm.host_url;
-    const flags = [
-        "--master",
-        `--host=${hostUrl}`,
-        "--csv=status"
-    ];
-    let expectSlaveCount: number;
-    if (slaveCount === 1) {
-        expectSlaveCount = 1;
-    } else if (slaveCount > 1 && slaveCount <= 5) {
-        expectSlaveCount = slaveCount - 1;
-    } else if (slaveCount > 5 && slaveCount <= 12) {
-        expectSlaveCount = slaveCount - 2;
-    } else {
-        expectSlaveCount = Math.floor(slaveCount * 0.85);
-    }
-    flags.push(`--users ${users}`);
-    flags.push(`--spawn-rate ${rate}`);
-    flags.push(`--run-time ${runTime}`);
-    flags.push("--headless");
-    flags.push(`--expect-workers=${expectSlaveCount}`);
-    const command = `ulimit -n 200000 && PYTHONWARNINGS="ignore:Unverified HTTPS request" nohup locust ${flags.join(" ")} > /dev/null 2>&1`;
-    console.log(`Executing ${command} on master at ${machine.ip_address} &`);
-    ssh.execCommand(command, {
-        options: {
-            pty: true
-        }
-    } as any);
-    await asyncSleep(10);
-    ssh.connection.end();
-    console.log(`Finished starting master at ${machine.ip_address}`);
-
-    console.log("Enqueuing slave start events...");
-    const promises = [];
-    for (const slaveMachineId of slaveIds) {
-        const slaveMachine = await Machine.findById(slaveMachineId);
-        const slaveProvisionEvent: MachineProvisionEvent = {
-            sshKey: { public: "", private: privateKey, created_at: new Date() },
-            eventType: WorkerEventType.MACHINE_PROVISION,
-            maxRetries: 10,
-            currentTry: 0,
-            lastActionTime: new Date(),
-            errors: [],
-            swarm,
-            machine: slaveMachine,
-            master: machine,
-            region: swarm.region,
-            stepToExecute: MachineSetupStep.START_SLAVE,
-            steps: []
-        };
-        promises.push(enqueue(slaveProvisionEvent));
-    }
-
+export async function startDataCollection(swarm: Swarm.Swarm, machine: Machine.Machine, slaveCount: number, slaveIds: number[], privateKey: string): Promise<void> {
     // Queue up periodic metrics capturing
     const fetchMetricsEvent: DataCaptureEvent = {
         sshKey: { public: "", private: privateKey, created_at: new Date() },
@@ -437,68 +347,9 @@ export async function startMaster(swarm: Swarm.Swarm, machine: Machine.Machine, 
         errors: [],
         swarm
     };
-    promises.push(enqueue(fetchMetricsEvent));
+    console.log("Enqueuing metrics start events");
+    await enqueue(fetchMetricsEvent);
 
-    await Promise.all(promises);
-    console.log("Done enqueuing slave start events.");
-}
-
-export async function startSlave(swarm: Swarm.Swarm, master: Machine.Machine, slave: Machine.Machine, privateKey: string): Promise<void> {
-    console.log("Transferring template to slave at ", slave.ip_address);
-    const bashTemplate = `
-        #!/bin/bash
-        export PYTHONWARNINGS="ignore:Unverified HTTPS request"
-        ulimit -n 200000 && locust --worker --master-host=${master.ip_address} --logfile=/root/locustlog.log --loglevel=debug &
-    `;
-    const bashPath = `/tmp/${slave.id}.bash`;
-    writeFileSync(`/tmp/${slave.id}.bash`, bashTemplate);
-    const sftp = new sftp_client();
-    await sftp.connect({
-        host: slave.ip_address,
-        port: 22,
-        username: "root",
-        privateKey
-    });
-    await sftp.put(bashPath, "/root/start.sh");
-
-    // Two worker processes on each machine.
-    for (let i = 1; i <= 2; i++) {
-        console.log(`Starting slave at ${slave.ip_address} on process ${i}`);
-        const ssh = new NodeSSH();
-        await ssh.connect({
-            host: slave.ip_address,
-            username: "root",
-            privateKey,
-        });
-        await ssh.execCommand("chmod +xrw /root/start.sh");
-        const command = "/bin/bash /root/start.sh";
-        console.log(`Executing ${command} on slave at ${slave.ip_address} on process ${i}`);
-        ssh.exec(command, [], {
-            cwd: "/root",
-            onStdout(chunk: any) {
-                console.log("stdoutChunk", chunk.toString("utf8"));
-            },
-            onStderr(chunk: any) {
-                console.log("stderrChunk", chunk.toString("utf8"));
-            },
-        });
-        await asyncSleep(15);
-        ssh.connection.end();
-    }
-
-    // Mark that the machine setup complete.
-    await Machine.updateSetupCompleteStatus(slave.id, true);
-
-    // Mark if the swarm setup is complete.
-    const machines: Machine.Machine[] = await SwarmMachine.getSwarmMachines(swarm.id);
-    const machineCount: number = machines.length - 1;
-    const setupCompleteCount: number = machines
-        .filter(m => !m.is_master)
-        .filter(m => m.setup_complete)
-        .length;
-    await Swarm.updateLoadTestStarted(swarm.id, !!(machineCount === setupCompleteCount));
-
-    console.log(`Finished starting slave at ${slave.ip_address}`);
 }
 
 export async function cleanUpMachineProvisionEvent(event: MachineProvisionEvent): Promise<void> {
