5f47e32a31b52f82c6cd7348515243418aab8f72
Jack Slingerland <jack.slingerland@gmail.com>
2022-12-22 06:20:31 -0500

    Removing optimizations

--- src/models/Route.ts
+++ src/models/Route.ts
@@ -1,26 +1,14 @@
 import { db } from "../lib/db";
 
-const localCache: any = {};
-let initialized = false;
-
 export interface Route {
     id?: number;
     route: string;
 }
 
 export async function getByName(route: string): Promise<Route> {
-    if (localCache["route"]) {
-        return {
-            id: localCache["route"],
-            route
-        };
-    } else {
-        const result = await db<Route>("route")
-            .where({ route })
-            .first();
-        localCache[result.route] = result.id;
-        return result;
-    }
+    return await db<Route>("route")
+        .where({ route })
+        .first();
 
 }
 
@@ -30,20 +18,10 @@ export async function create(path: string): Promise<Route> {
 }
 
 export async function getOrCreate(path: string): Promise<Route> {
-    // WIP - Cache for making migrations faster. Remove after.
-    if (!initialized) {
-        const allRoutes: Route[] = await db<Route>("route");
-        for (let i = 0; i < allRoutes.length; i++) {
-            localCache[`${allRoutes[i].route}`] = allRoutes[i].id;
-        }
-        initialized = true;
-    }
-    // WIP - check cache and create an entry
     const existingRoute = await getByName(path);
     if (existingRoute) {
         return existingRoute;
     } else {
-        // WIP - Add to cache and create entry.
         await create(path);
         return await getByName(path);
     }
