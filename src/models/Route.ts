57c6f0b2ea1fc9a7457300b8e1476f4086d5fb01
Jack Slingerland <jack.slingerland@gmail.com>
2022-12-22 06:13:54 -0500

    local caching for migration

--- src/models/Route.ts
+++ src/models/Route.ts
@@ -1,14 +1,27 @@
 import { db } from "../lib/db";
 
+const localCache: any = {};
+let initialized = false;
+
 export interface Route {
     id?: number;
     route: string;
 }
 
 export async function getByName(route: string): Promise<Route> {
-    return await db<Route>("route")
-        .where({ route })
-        .first();
+    if (localCache["route"]) {
+        return {
+            id: localCache["route"],
+            route
+        };
+    } else {
+        const result = await db<Route>("route")
+            .where({ route })
+            .first();
+        localCache[result.route] = result.id;
+        return result;
+    }
+
 }
 
 export async function create(path: string): Promise<Route> {
@@ -17,10 +30,20 @@ export async function create(path: string): Promise<Route> {
 }
 
 export async function getOrCreate(path: string): Promise<Route> {
+    // WIP - Cache for making migrations faster. Remove after.
+    if (!initialized) {
+        const allRoutes: Route[] = await db<Route>("route");
+        for (let i = 0; i < allRoutes.length; i++) {
+            localCache[`${allRoutes[i].route}`] = allRoutes[i].id;
+        }
+        initialized = true;
+    }
+    // WIP - check cache and create an entry
     const existingRoute = await getByName(path);
     if (existingRoute) {
         return existingRoute;
     } else {
+        // WIP - Add to cache and create entry.
         await create(path);
         return await getByName(path);
     }
