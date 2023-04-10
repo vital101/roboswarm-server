cd09ec09e751690ecb0c17322b78fd7b9543a243
Jack Slingerland <jack.slingerland@gmail.com>
2023-04-10 07:23:09 -0400

    Deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,20 @@
 
 # Fetch latest code, build
 cd /home/jack/repos/roboswarm-server
+echo "Fetching latest code..."
 git pull
+echo "Building Roboswarm."
 npm run build
+echo "Running database migrations"
 npm run migrate
 
 # Restart API and Worker
+echo "Restarting API and worker."
 pm2 restart all
 
 # Clear Nginx static assets cache
+echo "Clearing Nginx static asset cache."
 sudo su
 cd /var/cache/nginx
 rm -rf *
+echo "Done deploying Roboswarm Server"
